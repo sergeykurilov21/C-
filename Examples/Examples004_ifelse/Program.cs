daf1d5d0a299c46dfd069d4bbd1a7d22686d6368
Sergey <sergey.kurilov@mail.ru>
2023-01-28 17:08:02 +0300

    Examples004_ifelse

---
+++ Examples/Examples004_ifelse/Program.cs
@@ -0,0 +1,11 @@
+﻿
+Console.WriteLine("Введите имя пользователя");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "маша"){
+    Console.WriteLine("Ура это же МАША!");
+}
+else{
+    Console.Write("Привети, ");
+    Console.Write(username);
+    }
